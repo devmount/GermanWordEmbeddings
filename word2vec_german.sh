0d5c9d4210ec1400340580b3246c94f9216c69ed
Andreas M <mail@devmount.de>
2015-07-10 07:22:13 +0200

    removed: filter quotation

--- word2vec_german.sh
+++ word2vec_german.sh
@@ -8,7 +8,6 @@ mkdir model
 for i in 2007 2008 2009 2010 2011 2012 2013; do
 	wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.$i.de.shuffled.gz
 	gzip -d news.$i.de.shuffled.gz
-	sed -i 's|["'\''„“‚‘]||g' news.$i.de.shuffled
 	python preprocessing.py news.$i.de.shuffled corpus_ps/news.$i.de.shuffled.corpus -ps
 done
 rm news*
@@ -21,7 +20,6 @@ find extracted -name '*bz2' \! -exec bzip2 -k -c -d {} \; > dewiki.xml
 printf "Number of articles: "
 grep -o "<doc" dewiki.xml | wc -w
 sed -i 's/<[^>]*>//g' dewiki.xml
-sed -i 's|["'\''„“‚‘]||g' dewiki.xml
 rm -rf extracted
 python preprocessing.py dewiki.xml corpus_ps/dewiki.corpus -ps
 rm dewiki.xml
