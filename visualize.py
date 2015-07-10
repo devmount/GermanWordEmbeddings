971ce0003ac3b42ca0223004aa2b0dc947599d65
Andreas M <mail@devmount.de>
2015-07-10 16:41:50 +0200

    improved visualization

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@
 # @author: Andreas Mueller
 # @see: Bachelor Thesis 'Analyse von Wort-Vektoren deutscher Textkorpora'
 # 
-# @example: python visualize.py test.model -cu
+# @example: python visualize.py
 
 import gensim
 import matplotlib.pyplot as plt
@@ -15,7 +15,7 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 # configuration
-currency = ["Schweiz","Franken","Deutschland","Euro","Grossbritannien","britische_Pfund","Japan","Yen","Russland","Rubel","USA","US-Dollar","Kroatien","Kuna"] #"Pakistan","Rupien","China","Yuan",
+currency = ["Schweiz","Franken","Deutschland","Euro","Grossbritannien","britische_Pfund","Japan","Yen","Russland","Rubel","USA","US-Dollar","Kroatien","Kuna"]
 capital  = ["Athen","Griechenland","Berlin","Deutschland","Ankara","Tuerkei","Bern","Schweiz","Hanoi","Vietnam","Lissabon","Portugal","Moskau","Russland","Stockholm","Schweden","Tokio","Japan","Washington","USA"]
 language = ["Deutschland","Deutsch","USA","Englisch","Frankreich","Franzoesisch","Griechenland","Griechisch","Norwegen","Norwegisch","Schweden","Schwedisch","Polen","Polnisch","Ungarn","Ungarisch"]
 # matches = model.most_similar(positive=["Frau"], negative=[], topn=30)
@@ -25,9 +25,15 @@ language = ["Deutschland","Deutsch","USA","Englisch","Frankreich","Franzoesisch"
 # ... reduces dimensionality of vectors of given words either with PCA or with t-SNE and draws the words into a diagram
 # @param word2vec model     to visualize vectors from
 # @param list     words     list of word strings to visualize
-# @param bool     alternate use different color and align for every second word
 # @param bool     pca       use PCA (True) or t-SNE (False) to reduce dimensionality 
-def draw_words(model, words, alternate=False, pca=True, title=''):
+# @param bool     alternate use different color and label align for every second word
+# @param bool     arrows    use arrows to connect related words (items that are next to each other in list)
+# @param float    x1        x axis range (from)
+# @param float    x2        x axis range (to)
+# @param float    y1        y axis range (from)
+# @param float    y2        y axis range (to)
+# @param string   title     for diagram
+def draw_words(model, words, pca=False, alternate=True, arrows=True, x1=3, x2=3, y1=3, y2=3, title=''):
     # get vectors for given words from model
     vectors = [model[word] for word in words]
 
@@ -39,17 +45,14 @@ def draw_words(model, words, alternate=False, pca=True, title=''):
         vectors2d = tsne.fit_transform(vectors)
 
     # draw image
-    plt.figure()
-    plt.axis([-2.2,2.2,-2.2,2.2])
+    plt.figure(figsize=(6,6))
+    if pca:
+        plt.axis([x1, x2, y1, y2])
 
     first = True # color alternation to divide given groups
     for point, word in zip(vectors2d , words):
         # plot points
-        plt.scatter(
-            point[0],
-            point[1],
-            c='r'
-        )
+        plt.scatter(point[0], point[1], c='r' if first else 'g')
         # plot word annotations
         plt.annotate(
             word, 
@@ -63,30 +66,32 @@ def draw_words(model, words, alternate=False, pca=True, title=''):
         first = not first if alternate else first
 
     # draw arrows
-    for i in xrange(0, len(words)-1, 2):
-        a = vectors2d[i][0] + 0.04
-        b = vectors2d[i][1]
-        c = vectors2d[i+1][0] - 0.04
-        d = vectors2d[i+1][1]
-        plt.arrow(a, b, c-a, d-b,
-            shape='full',
-            lw=0.1,
-            edgecolor='#bbbbbb',
-            facecolor='#bbbbbb',
-            length_includes_head=True,
-            head_width=0.08,
-            width=0.01
-        )
+    if arrows:
+        for i in xrange(0, len(words)-1, 2):
+            a = vectors2d[i][0] + 0.04
+            b = vectors2d[i][1]
+            c = vectors2d[i+1][0] - 0.04
+            d = vectors2d[i+1][1]
+            plt.arrow(a, b, c-a, d-b,
+                shape='full',
+                lw=0.1,
+                edgecolor='#bbbbbb',
+                facecolor='#bbbbbb',
+                length_includes_head=True,
+                head_width=0.08,
+                width=0.01
+            )
 
     # draw diagram title
     if title:
         plt.title(title)
 
+    plt.tight_layout()
     plt.show()
 
 # get trained model
 model = gensim.models.Word2Vec.load_word2vec_format("model/SG-300-5-NS10-R50.model", binary=True)
-# execute evaluation
-draw_words(model, currency, True, True, r'$PCA\ Visualisierung:\ W\ddot{a}hrung$')
-draw_words(model, capital, True, True, r'$PCA\ Visualisierung:\ Hauptstadt$')
-draw_words(model, language, True, True, r'$PCA\ Visualisierung:\ Sprache$')
+# draw pca plots
+draw_words(model, currency, True, True, True, -3, 3, -2, 2, r'$PCA\ Visualisierung:\ W\ddot{a}hrung$')
+draw_words(model, capital, True, True, True, -3, 3, -2, 2.2, r'$PCA\ Visualisierung:\ Hauptstadt$')
+draw_words(model, language, True, True, True, -3, 3, -2, 1.7, r'$PCA\ Visualisierung:\ Sprache$')
